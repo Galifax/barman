64e672e9d23221a5a2e64d932204a98f77b9b12a
xzACzx <zAC95zUA@gmail.com>
2017-08-17 14:16:45 +0300

    tyest

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'hugOhP9_hNjgnvGeWyncHrdwt3IFnytu',
+            'cookieValidationKey' => 'yzz3HCVDq6yEF-8kihvN4hJ-_Isqg5fl',
         ],
     ],
 ];

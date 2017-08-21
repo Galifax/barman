74d374a5b507074b9e3cddff4c40c92327da781a
sadsenpai <sad.senpai.2001@gmail.com>
2017-08-21 15:38:57 +0300

    make main promo section responsive

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,11 +10,15 @@ $this->title = 'Barmen';
             <h1 class="main-promo__title">Онлайн-сервис заказа услуг</h1>
             <p class="main-promo__subtitle">Более 100 000 проверенных специалистов для выполнения ваших бытовых или бизнес задач Найти специалиста </p>
             <div class="main-promo__search">
-                <form action="/task/ceate">
-                    <input type="text" placeholder="Что нужно сделать?">
-                    <button class="b-button">
-                        <span>Найти специалиста</span>
-                    </button>
+                <form class="clearfix" action="/task/ceate">
+                    <div class="main-promo__search-button">
+                        <button class="b-button">
+                            <span>Найти специалиста</span>
+                        </button>
+                    </div>
+                    <div class="main-promo__search-field">
+                        <input type="text" placeholder="Что нужно сделать?">
+                    </div>
                 </form>
                 <div class="main-promo__search-hint">
                     Например:

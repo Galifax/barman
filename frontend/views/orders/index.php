893571b41f75cbdb3757bf2d193642fa3cfa0077
LtFrager <sadesade2011@gmail.com>
2017-09-06 19:14:20 +0300

    1

--- frontend/views/orders/index.php
+++ frontend/views/orders/index.php
@@ -5,65 +5,67 @@
 $this->title = 'Barmen';
 ?>
 
-<div class="container">
-    <div class="row" style=" margin:20px">
-        <div class="col-xs-12">
-            <button class="btn btn-small btn-danger" data-toggle="portfilter" data-target="all">
-                All
-            </button>
-            <button class="btn btn-small btn-danger" data-toggle="portfilter" data-target="art">
-                Websites
-            </button>
-            <button class="btn btn-small btn-danger" data-toggle="portfilter" data-target="media">
-                Brochures
-            </button>
-            <button class="btn btn-small btn-danger" data-toggle="portfilter" data-target="brand">
-                Logo design / Brand Collaterals
-            </button>
-        </div>
-    </div>
+<div class="col-sm-9">
 
-    <div class="col-md-4" data-tag='brand'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+
+    <div class="col-md-3" data-tag='brand'>
+        <img src="https://newtambov.ru/wp-content/uploads/taisia/2013/05/789.jpg" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='media'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='media'>
+        <img src="http://i95.beon.ru/99/8/1860899/0/60442300/1232641736_1232638447_203363_202111.jpeg" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='art'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='art'>
+        <img src="http://www.porjati.ru/uploads/posts/2011-09/thumbs/1317102257_zastavka.jpg" class="img-responsive" alt="img">
     </div>
 
 
 
 
-    <div class="col-md-4" data-tag='art'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='art'>
+        <img src="http://img2.goodfon.ru/wallpaper/big/7/4b/world-of-tanks-sovetskiy.jpg" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='brand'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='brand'>
+        <img src="https://img2.goodfon.ru/wallpaper/big/d/61/art-tank-t-40-sssr.jpg" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='media'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='media'>
+        <img src="http://www.fonstola.ru/mini/201412/155115.jpg" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='media'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='media'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX1LDsmmk_w868n0u7ZDqkEe_GSZ2NpK1QvhnJ2ooWR8U7VEQc" class="img-responsive" alt="img">
     </div>
-    <div class="col-md-4" data-tag='brand'>
-        <img src="http://placehold.it/450x250" class="img-responsive" alt="img">
+    <div class="col-md-3" data-tag='brand'>
+        <img src="http://top.scalemodels.ru/images/2012/01/1326470809_IMG_0297.jpg" class="img-responsive" alt="img">
     </div>
 
 
 </div>
 
-<script>
-    !function(d){var c="portfilter";
-    var b=function(e){this.$element=d(e);
-    this.stuff=d("[data-tag]");
-    this.target=this.$element.data("target")||""};
-    b.prototype.filter=function(g){var e=[],f=this.target;
-    this.stuff.fadeOut("fast").promise().done(function(){d(this).each(function(){if(d(this).data("tag")==f||f=="all"){e.push(this)}});
-    d(e).show()})};var a=d.fn[c];d.fn[c]=function(e){return this.each(function(){var g=d(this),f=g.data(c);
-    if(!f){g.data(c,(f=new b(this)))}if(e=="filter"){f.filter()}})};
-    d.fn[c].defaults={};d.fn[c].Constructor=b;d.fn[c].noConflict=function(){d.fn[c]=a;return this};d(document).on("click.portfilter.data-api","[data-toggle^=portfilter]",function(f){d(this).portfilter("filter")})}(window.jQuery);
-
-</script>
+<div class="col-sm-3">
+    <h3>Выберите категорию</h3>
+    <div class="row" style=" margin:20px">
+        <div class="col-sm-12">
+            <ul>
+                <li>
+                    <a class="btn btn-small " data-toggle="portfilter" data-target="all">
+                        Все
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-small " data-toggle="portfilter" data-target="art">
+                        Бар
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-small " data-toggle="portfilter" data-target="media">
+                        Ресторан
+                    </a>
+                </li>
+                <li>
+                    <a class="btn btn-small" data-toggle="portfilter" data-target="brand">
+                        Кафе
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>

aeccc9089fa471579ef7c22723e72f2d1208a86a
LtFrager <sadesade2011@gmail.com>
2017-09-06 20:49:42 +0300

    Orders-Beta

--- frontend/views/orders/index.php
+++ frontend/views/orders/index.php
@@ -5,67 +5,303 @@
 $this->title = 'Barmen';
 ?>
 
-<div class="col-sm-9">
+<div class="col-sm-9" style="margin-top: 15px;">
+    <div class="row">
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data1">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data1</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data1">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data1</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data2">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data2</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data2">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data2</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data2">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data2</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data2">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data2</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data3">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data3</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-order col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data3">
+            <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+                <p style="text-align:center; margin-top:20px;">
+                    <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+                </p>
+                <div class="caption">
+                    <div class="blur"></div>
+                    <div class="caption-text">
+                        <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data3</h3>
+                        <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                        <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                    </div>
+                </div>
+            </div>        </div>
+
+        <div class="categiry-ordert col-lg-4 col-md-4 col-sm-4 col-xs-6 filter Data3">
+        <div class="cuadro_intro_hover " style="background-color:#cccccc;">
+            <p style="text-align:center; margin-top:20px;">
+                <img src="http://placehold.it/500x330" class="img-responsive" alt="">
+            </p>
+            <div class="caption">
+                <div class="blur"></div>
+                <div class="caption-text">
+                    <h3 style="border-top:2px solid white; border-bottom:2px solid white; padding:10px;">Data3</h3>
+                    <p>Loren ipsum dolor si amet ipsum dolor si amet ipsum dolor...</p>
+                    <a class=" btn btn-default" href="#"><span class="glyphicon glyphicon-plus"> INFO</span></a>
+                </div>
+            </div>
+        </div>        </div>
+    </div>
 
 
-    <div class="col-md-3" data-tag='brand'>
-        <img src="https://newtambov.ru/wp-content/uploads/taisia/2013/05/789.jpg" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='media'>
-        <img src="http://i95.beon.ru/99/8/1860899/0/60442300/1232641736_1232638447_203363_202111.jpeg" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='art'>
-        <img src="http://www.porjati.ru/uploads/posts/2011-09/thumbs/1317102257_zastavka.jpg" class="img-responsive" alt="img">
-    </div>
 
 
+            <h2 class="border">Category order : *Category*</h2>
 
+            <div class="border">
+                <h2>Employer name</h2>
+                <img style="max-height: 100px" src="https://t3.ftcdn.net/jpg/01/06/07/16/240_F_106071621_UwCztl7yyMbVNSMijfuYyZrzbtmoxJPH.jpg" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+                <article>
+                    <p>Work type</p>
+                    <p>Duration of work</p>
+                    <p>
+                       Order discription | Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+                        five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                        of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                        Lorem Ipsum.
+                    </p></article>
+                <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Price 2000 $<br> Click to view details</a>
+            </div>
 
-    <div class="col-md-3" data-tag='art'>
-        <img src="http://img2.goodfon.ru/wallpaper/big/7/4b/world-of-tanks-sovetskiy.jpg" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='brand'>
-        <img src="https://img2.goodfon.ru/wallpaper/big/d/61/art-tank-t-40-sssr.jpg" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='media'>
-        <img src="http://www.fonstola.ru/mini/201412/155115.jpg" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='media'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX1LDsmmk_w868n0u7ZDqkEe_GSZ2NpK1QvhnJ2ooWR8U7VEQc" class="img-responsive" alt="img">
-    </div>
-    <div class="col-md-3" data-tag='brand'>
-        <img src="http://top.scalemodels.ru/images/2012/01/1326470809_IMG_0297.jpg" class="img-responsive" alt="img">
-    </div>
+            <div class="border">
+            <h2>Employer name</h2>
+            <img style="max-height: 100px" src="https://t3.ftcdn.net/jpg/01/06/07/16/240_F_106071621_UwCztl7yyMbVNSMijfuYyZrzbtmoxJPH.jpg" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+            <article>
+                <p>Work type</p>
+                <p>Duration of work</p>
+                <p>
+                    Order discription | Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum.
+                </p></article>
+            <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Price 2000 $<br> Click to view details</a>
+            </div>
 
+            <div class="border">
+            <h2>Employer name</h2>
+            <img style="max-height: 100px" src="https://t3.ftcdn.net/jpg/01/06/07/16/240_F_106071621_UwCztl7yyMbVNSMijfuYyZrzbtmoxJPH.jpg" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+            <article>
+                <p>Work type</p>
+                <p>Duration of work</p>
+                <p>
+                    Order discription | Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum.
+                </p></article>
+            <a class="btn btn-blog pull-right marginBottom10" href="http://bootsnipp.com/user/snippets/2RoQ">Price 2000 $<br> Click to view details</a>
+            </div>
+
+            <div class="col-md-12 gap10"></div>
 
 </div>
 
-<div class="col-sm-3">
-    <h3>Выберите категорию</h3>
+
+<div class="col-sm-3" style="height: 100%">
     <div class="row" style=" margin:20px">
         <div class="col-sm-12">
-            <ul>
+            <h3 class="texe-tenter">Выберите категорию</h3>
+            <ul class="filter-list">
                 <li>
-                    <a class="btn btn-small " data-toggle="portfilter" data-target="all">
+                    <button class="btn btn-default filter-button" data-filter="all">
                         Все
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a class="btn btn-small " data-toggle="portfilter" data-target="art">
+                    <button class="btn btn-default filter-button" data-filter="Data1">
                         Бар
-                    </a>
+                    </button>
                 </li>
                 <li>
-                    <a class="btn btn-small " data-toggle="portfilter" data-target="media">
+                    <button class="btn btn-default filter-button" data-filter="Data2">
                         Ресторан
+                    </button>
+                </li>
+                <li>
+                    <button class="btn btn-default filter-button" data-filter="Data3">
+                        Кафе
+                    </button>
+                </li>
+            </ul>
+
+            <hr>
+
+            <h3 class="text-center">Other cities</h3>
+            <ul class="other-cities">
+                <li>
+                    <a href="/" class="">
+                        citie 1
                     </a>
                 </li>
                 <li>
-                    <a class="btn btn-small" data-toggle="portfilter" data-target="brand">
-                        Кафе
+                    <a href="/" class="">
+                        citie 2
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 3
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 4
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 5
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 6
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 7
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie 8
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="">
+                        citie N
                     </a>
                 </li>
             </ul>
-        </div>
+
+            <hr>
+
+                <h3 class="text-center">Tell About Us</h3>
+                <p>We provide jobs to thousands of Ukrainians. Tell your friends about it and get a plus in your karma!</p>
+
+                <h3 class="text-center">Follow:</h3>
+                                    <a href="" id="gh" target="_blank" title="Twitter"><span class="fa-stack fa-lg">
+                      <i class="fa fa-square-o fa-stack-2x"></i>
+                      <i class="fa fa-twitter fa-stack-1x"></i>
+                    </span>
+                                        Twitter</a>
+            <br>
+                                    <a  href=""  target="_blank" title="GitHub"><span class="fa-stack fa-lg">
+                      <i class="fa fa-square-o fa-stack-2x"></i>
+                      <i class="fa fa-facebook fa-stack-1x"></i>
+                    </span>
+                    Facebook</a>
+
+            <hr>
+
+            <p>Place for advertisement</p>
+
     </div>
 </div>
+
